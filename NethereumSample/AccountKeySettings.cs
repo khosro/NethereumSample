36885bc9625a061bde41699254963af9c225a141
kh.asghari <kh.asghari@keivangroup.com>
2019-09-21 18:13:30 +0430

    .

---
+++ NethereumSample/AccountKeySettings.cs
@@ -0,0 +1,22 @@
+﻿using NBitcoin;
+
+namespace NethereumSample
+{
+    public class AccountKeySettings
+    {
+        public HDFingerprint? RootFingerprint { get; set; }
+        public KeyPath AccountKeyPath { get; set; }
+
+        public RootedKeyPath GetRootedKeyPath()
+        {
+            if (RootFingerprint is HDFingerprint fp && AccountKeyPath != null)
+                return new RootedKeyPath(fp, AccountKeyPath);
+            return null;
+        }
+        public BitcoinExtPubKey AccountKey { get; set; }
+        public bool IsFullySetup()
+        {
+            return AccountKeyPath != null && RootFingerprint is HDFingerprint;
+        }
+    }
+}
--- NethereumSample/Program.cs
+++ NethereumSample/Program.cs
@@ -1,15 +1,19 @@
 ﻿using NBitcoin;
+using NBXplorer.DerivationStrategy;
 using Nethereum.HdWallet;
+using Nethereum.Hex.HexConvertors.Extensions;
 using Nethereum.Hex.HexTypes;
 using Nethereum.RPC.Eth.DTOs;
 using Nethereum.Web3;
 using Nethereum.Web3.Accounts;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Numerics;
 using System.Threading;
 using System.Threading.Tasks;
+
 namespace NethereumSample
 {
     internal class Program
@@ -21,14 +25,58 @@ namespace NethereumSample
 
             //SyncStatus().GetAwaiter().GetResult();
 
-            HdWallet();
+            //HdWallet();
+
+            HdWallet1();
+
+            Drive();
 
             Console.ReadLine();
         }
 
+        #region From BtcPayserver
+        static void Drive()
+        {
+            string masterPublickey = "xpub6EadKVNnpzBqV11L1NtEnxik6awzxxmFmGisfZjW73b45iwJJ3LRuZqHZLJodWzja1F2cPQAAMCRkGuBqWm4N21fMM1groibrd7A5nZfSRJ-[p2sh]";
+            //  masterPublickey = "xpub6EadKVNnpzBqV11L1NtEnxik6awzxxmFmGisfZjW73b45iwJJ3LRuZqHZLJodWzja1F2cPQAAMCRkGuBqWm4N21fMM1groibrd7A5nZfSRJ-[p2sh]";
+            // masterPublickey = "0xf20cAf38A4B1C322b386f58FcC43eeE6915caa0F";
+            //   masterPublickey = "tpubDExFqjCUYG9HkoCBTZsKzt47Ri3exMGKJuKB27neSxLwq1c1NGBX41W7oNPbA1wyMumwDxv2hT2USTPoENcJFTTxQwkzYLNqSahZqqMekzo-[p2sh]";
+            // masterPublickey = "tpubDExFqjCUYG9HkoCBTZsKzt47Ri3exMGKJuKB27neSxLwq1c1NGBX41W7oNPbA1wyMumwDxv2hT2USTPoENcJFTTxQwkzYLNqSahZqqMekzo";
+            Network network = Network.Main;
+            var parser = new DerivationSchemeParser(network);
+
+            var derivationSchemeSettings = new DerivationSchemeSettings(parser.Parse(masterPublickey), network);
+
+            ShowAddresses(derivationSchemeSettings, network);
+        }
+        private static void ShowAddresses(DerivationSchemeSettings strategy, Network network)
+        {
+            Dictionary<string, string> keyPath2Addresses = new Dictionary<string, string>();
+
+            var deposit = new NBXplorer.KeyPathTemplates(null).GetKeyPathTemplate(DerivationFeature.Deposit);
+            //  var deposit = new NBXplorer.KeyPathTemplates(new NBXplorer.KeyPathTemplate(new NBitcoin.KeyPath("m/44'/60'/0'/0"), new NBitcoin.KeyPath("m/44'/60'/0'/0"))).GetKeyPathTemplate(DerivationFeature.Deposit);
+            //var deposit = new NBXplorer.KeyPathTemplates(new NBXplorer.KeyPathTemplate(new NBitcoin.KeyPath("m/44'/60'/0'/0"), new NBitcoin.KeyPath("m/44'/60'/0'/0"))).GetKeyPathTemplate(new NBitcoin.KeyPath("m/44'/60'/0'/0"));
+
+            var line = strategy.AccountDerivation.GetLineFor(deposit);
+
+            //var line1 = strategy.AccountDerivation.GetDerivation(new NBitcoin.KeyPath("m/44/60/0/0"));
+            var d = strategy.AccountDerivation.GetDerivation(new NBitcoin.KeyPath("m/44/60/0/0/2")).ScriptPubKey.GetDestinationAddress(network).ToString();
+            Console.WriteLine("d ::: " + d);
+            for (int i = 0; i < 10; i++)
+            {
+                var address = line.Derive((uint)i);
+                string keyPath = deposit.GetKeyPath((uint)i).ToString();
+                string destinationAddress = address.ScriptPubKey.GetDestinationAddress(network).ToString();
+                Console.WriteLine($" keyPath {keyPath} , destinationAddress {destinationAddress}");
+            }
+        }
+        #endregion
+
+        #region Ethereum Hd Wallet
         private static void HdWallet()
         {
             string words = "ripple scissors kick mammal hire column oak again sun offer wealth tomorrow wagon turn fatal";
+
             string password = "";
             Wallet wallet1 = new Wallet(words, password);
             Console.WriteLine($"Path : {wallet1.Path}");
@@ -52,13 +100,42 @@ namespace NethereumSample
             ExtKey masterKey = extKey.Derive(new NBitcoin.KeyPath("m/44'/60'/0'/0"));
             string masterPublicKey = masterKey.Neuter().GetWif(net).ToString();
             string masterPrivateKey = masterKey.GetWif(net).ToString();
-            
-            
+
+
             Console.WriteLine($"");
             Console.WriteLine($"MasterPrivateKey : {net.ToString()} {masterPrivateKey} ");
             Console.WriteLine($"MasterPublicKey  : {net.ToString()} {masterPublicKey} ");
             Console.WriteLine($"");
         }
+        #endregion
+
+
+        #region  From Nethereum 
+
+        static void HdWallet1()
+        {
+            ExtKey d = new ExtKey();
+
+
+            string Path = "m/44'/60'/0'/0/x";
+            string words = "ripple scissors kick mammal hire column oak again sun offer wealth tomorrow wagon turn fatal";
+              words = "great cash inform total move frozen chase imitate ten shell raven elegant";
+            var mneumonic = new Mnemonic(words);
+            var seed = mneumonic.DeriveSeed("123456@a").ToHex();
+            var masterKey = new ExtKey(seed);
+            var d1 = new Account(masterKey.PrivateKey.ToBytes()).Address;
+            for (int i = 0; i < 10; i++)
+            {
+                var keyPath = new NBitcoin.KeyPath(Path.Replace("x", i.ToString()));
+                ExtKey extKey = masterKey.Derive(keyPath);
+                byte[] privateKey = extKey.PrivateKey.ToBytes();
+                var account = new Account(privateKey);
+ 
+                Console.WriteLine("Account index : " + i + " - Address : " + account.Address + " - Private key : " + account.PrivateKey);
+            }
+        }
+
+        #endregion
 
 
 
